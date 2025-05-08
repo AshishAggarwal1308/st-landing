1190974c87cad48e1fb30e69f3bca4804d8afcfa
Ashish Aggarwal <ashu2000aggarwal@gmail.com>
2025-05-08 17:42:20 +0530

    add google pixel code

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 import Image from "next/image";
+import Script from "next/script";
 
 export default function Home() {
   return (
 <>
+        <Script
+                strategy="afterInteractive"
+                src="https://www.googletagmanager.com/gtag/js?id=AW-16491466128"
+            />
+            <Script id="google-ads" strategy="afterInteractive">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16491466128');
+        `}
+            </Script>
 </>
   );
 }
