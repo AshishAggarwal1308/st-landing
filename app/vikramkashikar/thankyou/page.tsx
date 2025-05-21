49187145f7accc7c1f69addbcf314f45bc74bd06
Ashish Aggarwal <ashu2000aggarwal@gmail.com>
2025-05-21 16:42:39 +0530

    updated pixel code of vikram

--- app/vikramkashikar/thankyou/page.tsx
+++ app/vikramkashikar/thankyou/page.tsx
@@ -43,8 +43,9 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1236864701103548');
+fbq('init', '1213866337065764');
 fbq('track', 'PageView');
+
 
         `}
       </Script>
@@ -53,7 +54,7 @@ fbq('track', 'PageView');
           height="1"
           width="1"
           style={{ display: 'none' }}
-          src="https://www.facebook.com/tr?id=1236864701103548&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=1213866337065764&ev=PageView&noscript=1"
         />
       </noscript>
       <title>Stock Tutor - Thankyou</title>
