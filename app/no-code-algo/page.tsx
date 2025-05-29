0fe5e33eb37878e2f2f78c55a8d42b8547074abe
Ashish Aggarwal <ashu2000aggarwal@gmail.com>
2025-05-29 17:29:19 +0530

    updated tabola code

--- app/no-code-algo/page.tsx
+++ app/no-code-algo/page.tsx
@@ -307,7 +307,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1834827303964862');
+fbq('init', '10041111282577074');
 fbq('track', 'PageView');
 
         `}
@@ -317,7 +317,7 @@ fbq('track', 'PageView');
                     height="1"
                     width="1"
                     style={{ display: 'none' }}
-                    src="https://www.facebook.com/tr?id=1834827303964862&ev=PageView&noscript=1"
+                    src="https://www.facebook.com/tr?id=10041111282577074&ev=PageView&noscript=1"
 
                 />
             </noscript>
