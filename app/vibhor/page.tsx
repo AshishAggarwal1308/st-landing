6d26585cc01d2a9d252c6084dc831610a77d1ee9
Ashish Aggarwal <ashu2000aggarwal@gmail.com>
2025-05-29 16:51:04 +0530

    updated tabola code

--- app/vibhor/page.tsx
+++ app/vibhor/page.tsx
@@ -249,29 +249,23 @@ function Page() {
 
     return (
         <>
-            <Head>
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-              window._tfa = window._tfa || [];
-              window._tfa.push({notify: 'event', name: 'page_view', id: 1855234});
-              !function (t, f, a, x) {
-                if (!document.getElementById(x)) {
+            <Script id="taboola-pixel" strategy="afterInteractive">
+                {`
+          window._tfa = window._tfa || [];
+          window._tfa.push({notify: 'event', name: 'page_view', id: 1855234});
+          !function (t, f, a, x) {
+              if (!document.getElementById(x)) {
                   t.async = 1;
                   t.src = a;
                   t.id = x;
                   f.parentNode.insertBefore(t, f);
-                }
-              }(
-                document.createElement('script'),
-                document.getElementsByTagName('script')[0],
-                '//cdn.taboola.com/libtrc/unip/1855234/tfa.js',
-                'tb_tfa_script'
-              );
-            `,
-                    }}
-                />
-            </Head >
+              }
+          }(document.createElement('script'),
+            document.getElementsByTagName('script')[0],
+            '//cdn.taboola.com/libtrc/unip/1855234/tfa.js',
+            'tb_tfa_script');
+        `}
+            </Script>
 
             <Script id="meta-pixel" strategy="afterInteractive">
                 {`
