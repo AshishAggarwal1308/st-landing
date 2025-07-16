526adc9ed0ffd1a5497545969f6f3a1a1c1d8dfb
AshishAggarwal1308 <ashu2000aggarwal@gmail.com>
2025-07-16 14:42:14 +0530

    added meta pixel code on annie algo trading

--- app/annie-algo-trading/page.tsx
+++ app/annie-algo-trading/page.tsx
@@ -391,8 +391,29 @@ function Page() {
 
     return (
         <>
-            
+          <Script id="meta-pixel" strategy="afterInteractive">
+                {`
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '1690170988281403');
+                fbq('track', 'PageView');
 
+                `}
+            </Script>
+            <noscript>
+                <img
+                    height="1"
+                    width="1"
+                    style={{ display: 'none' }}
+                    src="https://www.facebook.com/tr?id=1690170988281403&ev=PageView&noscript=1"
+                />
+            </noscript>  
             <title>Free Masterclass: Learn No-Code Algo Trading</title>
             <meta
                 name="description"
